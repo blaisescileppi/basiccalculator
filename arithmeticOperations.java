5b0d19ca86512a8a673113bc34edf5e303f52251
Blaise Scileppi <bscileppi0917@gmail.com>
2024-12-16 19:31:14 -0500

    moving methods/ clearing out driver

--- arithmeticOperations.java
+++ arithmeticOperations.java
@@ -1,27 +1,71 @@
-import java.lang.Math;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class arithmeticOperations {    
-public double ans;
+// public double ans;
+public static arithmeticOperations output;
 
-    public double Add(double num1, double num2) {
-        ans = num1 + num2;
-        return ans;
+
+    static void intro() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Would you like to continue?");
+        System.out.println("1: Addition \n 2: Subtraction \n 3: Multiplication \n 4: Division \n 5: Quit");
+        int choice = scan.nextInt();
+        
+        if(choice == 1) {
+            output.Add();
+        } else if (choice == 2) {
+            output.Sub();
+        } else if (choice == 3) {
+            output.Mult();
+        } else if (choice == 4) {
+            output.Div();
+        } else {
+            System.out.println("Invalid. Please choose a different value.");
+        }
+    scan.close();
     }
 
-    public double Sub(double num1, double num2) {
-        ans = num1 - num2;
-        return ans;
+    public double Add() {
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Enter two numbers");
+            double num1 = scan.nextDouble();
+            double num2 = scan.nextDouble();
+
+            return num1 + num2;
+            // intro();
+        }
     }
 
-    public double Mult(double num1, double num2) {
-        ans = num1 * num2;
-        return ans;
+    public double Sub() {
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Enter two numbers");
+            double num1 = scan.nextDouble();
+            double num2 = scan.nextDouble();
+
+            return num1 - num2;
+        }
     }
 
-    public double Div(double num1, double num2) {
-        ans = num1/num2;
-        return ans;
+    public double Mult() {
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Enter two numbers");
+            double num1 = scan.nextDouble();
+            double num2 = scan.nextDouble();
+
+            return num1 * num2;
+        }
     }
 
+    public double Div() {
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Enter two numbers");
+            double num1 = scan.nextDouble();
+            double num2 = scan.nextDouble();
+
+            return num1/num2;
+        }
+    }
 }
 
--- calculator.java
+++ calculator.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.io.*;
 
 public class calculator {
 public static arithmeticOperations output;
@@ -7,26 +6,12 @@ public static arithmeticOperations output;
     Scanner scan = new Scanner(System.in);
 
     System.out.println("What operation would you like to calculate?");
-    System.out.println("1: Addition \n 2: Subtraction \n 3: Multiplication 4: Division");
+    System.out.println("1: Addition \n 2: Subtraction \n 3: Multiplication \n 4: Division \n 5: Quit");
     int choice = scan.nextInt();
 
-    System.out.println("Enter two numbers");
-    double num1 = scan.nextDouble();
-    double num2 = scan.nextDouble();
 
-
-    if(choice == 1) {
-        output.Add(num1, num2);
-    } else if (choice == 2) {
-        output.Sub(num1, num2);
-    } else if (choice == 3) {
-        output.Mult(num1, num2);
-    } else if (choice == 4) {
-        output.Div(num1, num2);
-    } else {
-        System.out.println("Invalid. Please choose a different value.");
-    }
-
+while (choice != 5){
+}
 // infix to post fix use stack for
 
 }
