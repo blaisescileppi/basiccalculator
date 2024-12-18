d1ce4f65c860187f6387e0307db5e8ca83554444
Blaise Scileppi <bscileppi0917@gmail.com>
2024-12-17 20:55:55 -0500

    Set up maven project, pom.xml, and added .gitignore to exclude generated files

--- src/main/java/arithmeticOperations.java
+++ src/main/java/arithmeticOperations.java
@@ -1,5 +1,7 @@
+package src.main.java;
 import java.util.Scanner;
 import java.util.regex.*;
+
 // import Jama.Matrix;
 
 public class arithmeticOperations {
@@ -43,29 +45,35 @@ public class arithmeticOperations {
         String eq1 = scan.nextLine();
         String eq2 = scan.nextLine();
         String eq3 = scan.nextLine();
+        double A, B, C, D, A2, B2, C2, D2, A3, B3, C3, D3;
 
         Pattern pattern = Pattern.compile("([+-]?\\d*\\.?\\d+)x\\s*([+-]\\s*\\d*\\.?\\d+)y\\s*([+-]\\s*\\d*\\.?\\d+)z\\s*=\\s*([+-]?\\d*\\.?\\d+)");
         Matcher matcher = pattern.matcher(eq1);
         if (matcher.find()) {
-            double A = Double.parseDouble(matcher.group(1));
-            double B = Double.parseDouble(matcher.group(2));
-            double C = Double.parseDouble(matcher.group(3));
-            double D = Double.parseDouble(matcher.group(4));
+            A = Double.parseDouble(matcher.group(1));
+            B = Double.parseDouble(matcher.group(2));
+            C = Double.parseDouble(matcher.group(3));
+            D = Double.parseDouble(matcher.group(4));
         }
         Matcher matcher2 = pattern.matcher(eq2);
         if (matcher2.find()) {
-            double A2 = Double.parseDouble(matcher2.group(1));
-            double B2 = Double.parseDouble(matcher2.group(2));
-            double C2 = Double.parseDouble(matcher2.group(3));
-            double D2 = Double.parseDouble(matcher2.group(4));
+            A2 = Double.parseDouble(matcher2.group(1));
+            B2 = Double.parseDouble(matcher2.group(2));
+            C2 = Double.parseDouble(matcher2.group(3));
+            D2 = Double.parseDouble(matcher2.group(4));
         }
         Matcher matcher3 = pattern.matcher(eq3);
         if (matcher3.find()) {
-            double A3 = Double.parseDouble(matcher3.group(1));
-            double B3 = Double.parseDouble(matcher3.group(2));
-            double C3 = Double.parseDouble(matcher3.group(3));
-            double D3 = Double.parseDouble(matcher3.group(4));
+            A3 = Double.parseDouble(matcher3.group(1));
+            B3 = Double.parseDouble(matcher3.group(2));
+            C3 = Double.parseDouble(matcher3.group(3));
+            D3 = Double.parseDouble(matcher3.group(4));
         }
+
+        double[][] coef = {{A, B, C}, {A2, B2, C2}, {A3, B3, C3}};
+        double[] constants = {D, D2, D3};
+
+        double detA = determinant(coef);
 
         return 0;
      }
